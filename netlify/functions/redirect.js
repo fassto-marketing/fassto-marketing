cc6f2b39066bf210642a82fb12f5bd0380a4ac9f
fassto-marketing <hello@fassto.com>
2025-05-13 12:09:32 +0900

    Update redirect.js

--- netlify/functions/redirect.js
+++ netlify/functions/redirect.js
@@ -1,21 +1,28 @@
-// shorten.js에서 저장된 urls 객체를 같은 파일 내 공유
-const urls = require('./shorten').urls;
+const fetch = require('node-fetch');
 
-exports.handler = async function (event) {
+exports.handler = async function(event) {
   const code = event.queryStringParameters.code;
-  const url = urls && urls[code];
 
-  if (!url) {
+  const res = await fetch(`${process.env.SUPABASE_URL}/rest/v1/urls?shortcode=eq.${code}`, {
+    headers: {
+      'apikey': process.env.SUPABASE_ANON_KEY,
+      'Authorization': `Bearer ${process.env.SUPABASE_ANON_KEY}`,
+    }
+  });
+
+  const data = await res.json();
+
+  if (!data || data.length === 0) {
     return {
       statusCode: 404,
-      body: '유효하지 않은 단축 URL입니다.',
+      body: '단축 URL을 찾을 수 없습니다.'
     };
   }
 
   return {
     statusCode: 302,
     headers: {
-      Location: url,
-    },
+      Location: data[0].original_url
+    }
   };
 };
